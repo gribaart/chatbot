d9dd886de673155f4c5feeeaf0e2607c4d4427fd
Artem Gribanov <gribaart@fel.cvut.cz>
2021-10-24 08:03:44 +0200

    Meta-personality of bot

---
+++ app/personality/assistent_personality.py
@@ -0,0 +1,68 @@
+initial_template = '"Psychologist:Hello! I am a virtual psychologist and your personal psychological assistant. ' \
+                   'I study nicotine addiction. ' \
+                   'I know a lot about the dangers of nicotine and cigarettes. I will help you cope with nicotine ' \
+                   'addiction. ' \
+                   'Me:I am your patient with nicotine addiction. Can you help me? ' \
+                   'Psychologist:I will help you with tips on how to cope with your cigarette cravings. ' \
+                   'Tell me about your problem or ask a question and I will answer you. ' \
+
+question_template = '"Answer:I have a problem with nicotine addiction. ' \
+                    'Question:How long have you been smoking? ' \
+                    'Answer:I have been smoking since childhood. ' \
+                    'Question:How many cigarettes do you smoke on average daily? ' \
+                    'Answer:My cravings for smoking depend on my psychological state. ' \
+
+suicidal_warning = 'Perhaps this is not a good idea! Try to contact the psychological support service. ' \
+                   '112 is similar to the American 911 general emergency service, active in all EU countries, ' \
+                   'and can be traced with caller ID.If you have a medical emergency (including acute psychiatric ' \
+                   'condition or suicidal state) an ambulance will arrive to take you to the nearest ' \
+                   'appropriate medical facility.'
+
+youtube_knowledge_base = [
+    'https://www.youtube.com/watch?v=o3I0mJ2RfU0&ab_channel=AsapSCIENCE',
+    'https://www.youtube.com/watch?v=vUx-b89laPU&ab_channel=WatchMojo.com',
+    'https://www.youtube.com/watch?v=HD__r66sFjk&ab_channel=ChrisNotap',
+    'https://www.youtube.com/watch?v=5zWB4dLYChM&ab_channel=CentersforDiseaseControlandPrevention%28CDC%29',
+    'https://www.youtube.com/watch?v=lW6hwmdZbmE&ab_channel=iheed',
+    'https://www.youtube.com/watch?v=sVrb3B5m99M&ab_channel=HuntsmanCancerInstitute',
+    'https://www.youtube.com/watch?v=w5c6yvZEQ7M&ab_channel=BaylorScott%26WhiteHealth'
+]
+
+tips_to_quit_smoking = [
+    "Take the paste and toothbrush in your hand and brush your teeth. Slowly and thoroughly ...",
+    "Drink water, juice or lemonade. Drink the whole glass slowly and concentrate on each sip.",
+    "Get rid of the taste in your mouth quickly. Take a menthol gum or candy and let it gradually release. Focus your attention on the taste you are currently feeling.",
+    "How about going to make tea? Mint, lemon balm or chamomile also have calming effects and improve digestion.",
+    "Take a deep breath ten times in a row. Close your eyes, inhale through your nose and exhale slowly through your mouth.",
+    "Have something small to eat. Take a break, prepare your favorite food and really enjoy it.",
+    "Pump up your body. Run up the stairs three times, do 5 squats or push-ups and exhale. Then you can continue with a clear head.",
+    "Play your favorite song that will kick you. Immerse yourself fully in the catchy melody.",
+    "Do you enjoy playing on your mobile phone? Open your favorite game and overcome the smoking break with a mobile phone in your hand.",
+    "What are your friends doing? Open Messenger and send a greeting to someone you love. You will get rid of the thought of a cigarette and make your loved ones happy.",
+    "Are you interested in cars, technology, fashion or cooking? Browse photos of your favorite topic on Instagram.",
+    "Sit in a quiet room. Take a breath, close your eyes and move your thoughts to your favorite place. It can be a forest, a beach or a family picnic. Concentrate on the sounds, smells and other stimuli of the environment in which you feel good.",
+    "What are your reasons for quitting? Repeat aloud why you want to quit smoking - one reason after another… Your reasons are stronger than the craving for cigarettes!",
+    "Do you miss the cigarette coating? Prepare a glass of your favorite non-alcoholic drink and drink it with a straw. It will confiscate your mouth and the craving for cigarettes will quickly disappear.",
+    "Use nicotine spray or nicotine gum. Leave the released nicotine in the mouth or spit it out. After swallowing, it would irritate the stomach.",
+    "Restless hands are great for a puzzle, anti-stress ball or plasticine. What will you choose from that?",
+    "Quickly change the activity you are doing. Leave the room for a while, open the window and water the flowers, stretch. Your brain needs to be entertained by another activity.",
+    "Mouthwash not only protects the teeth, but also literally kills the craving of the cigarette. Rinse your mouth thoroughly. Try to last at least 20 seconds.",
+    "Let me guess which fruit you like best - is it a banana? Or an apple? Run to the store, buy at least 3 pieces of fruit and enjoy them.",
+    "Do you remember the bubble blower from your childhood? Now it will come in handy again. Try to make the biggest bubble possible. You have 10 attempts.",
+    "The inflatable balloon can be used in other ways than at a birthday party. Try to inflate it as quickly as possible. You will practice your lung capacity and you won't even remember smoking.",
+    "Open today's newspaper or news website and read what's new. Politics, sports, celebrities ... Anything that interests you. Can you handle at least one whole article?",
+    "Yoga will help you reduce stress and drive away thoughts of smoking. In addition, it can be practiced both at home and in the office and outdoors. Let's face it - what about the position of a lotus flower?",
+    "Do you remember the obligatory recitations in primary school? Deliberately, if you still remember a poem. Look in your memory and try to recite at least three verses.",
+    "Is there music playing nearby? Start dancing as if no one has seen you. Don't be afraid to start it properly.",
+    "The smoking break lasts about 7 minutes. During that time, you can read a piece of a chapter of your favorite book. The perfect time to immerse yourself in the action!",
+    "Do you like funny videos on YouTube? What are we going to talk about - I like to watch them sometimes. Try to look at some. You will have fun and then the taste for cigarettes will not be so strong.",
+    "Do you have someone close to you? Embrace him properly, at least 20 seconds. You will see that you will feel better. Happiness hormones will be flushed out in your brain.",
+    "How about tormenting the brain threads and trying to solve a crossword puzzle or sudoku?",
+    "Grab crayons or felt-tip pens and get to work. Try anti-stress coloring books or awaken your own talent and imagination. Simply draw what comes to mind.",
+    "We usually have a lot of things on the desk and sometimes a nice mess. Now is the right time to clean the desk. You will entertain your mind and you will work better.",
+    "Go to the park or the forest for a while. Close your eyes and listen to everything that is happening around you - the birdsong, the rustling of leaves, the bubbling stream or the gurgling water in the fountain. Isn't that beautiful?",
+    "Take a quick cold shower. I bet you won't even think about smoking.",
+    "Is your fridge empty? Make a shopping list so you know what to buy. You won't starve!",
+    "Are you getting dirty clothes in your laundry basket? Put it in the washing machine with it!",
+    "Is your sink filled with dirty dishes? Put it in the dishwasher or wash it. It will help you to detach yourself from the thought of a cigarette."
+]
